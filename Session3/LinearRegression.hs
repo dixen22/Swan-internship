7092035d4c74abec640f47946549f492e8f6ecb7
Swan Denojean <swan.denojean@gmail.com>
2026-04-19 22:32:28 +0900

    session 3 v2

--- Session3/LinearRegression.hs
+++ Session3/LinearRegression.hs
@@ -1,11 +1,13 @@
-import Torch.Tensor (Tensor, asTensor, asValue)
+import Torch.Tensor (Tensor, asTensor)
 import Torch.Functional (matmul, add, transpose2D)
+import Torch.Control (mapAccumM)
 
 
-ys :: [Float]
-ys = [130, 195, 218, 166, 163, 155, 204, 270, 205, 127, 260, 249, 251, 158, 167]
-xs :: [Float]
-xs = [148, 186, 279, 179, 216, 127, 152, 196, 126, 78, 211, 259, 255, 115, 173]
+ys :: [Tensor]
+ys = map asTensor ([130, 195, 218, 166, 163, 155, 204, 270, 205, 127, 260, 249, 251, 158, 167] :: [Float])
+
+xs :: [Tensor]
+xs = map asTensor ([148, 186, 279, 179, 216, 127, 152, 196, 126, 78, 211, 259, 255, 115, 173] :: [Float])
 
 linear ::
     (Tensor, Tensor) -> -- ^ parameters ([a, b]: 1 × 2, c: scalar)
@@ -13,21 +15,27 @@ linear ::
     Tensor              -- ^ z: 1 × 10
 linear (slope, intercept) input = slope * input + intercept
 
-displayResults :: Show a => [(a, a)] -> IO ()
-displayResults [] = return ()
-displayResults ((groundTruth, estimatedY):l) = do
-    putStrLn ("correct answer: " ++ show groundTruth)
-    putStrLn ("estimated: " ++ show estimatedY)
+displayStep :: Tensor -> Tensor -> IO ()
+displayStep groundTruth estimatedY = do
+    putStrLn $ "correct answer: " ++ show groundTruth
+    putStrLn $ "estimated: " ++ show estimatedY
     putStrLn "******"
-    displayResults l
+    return ()
+
+linearStep :: (Tensor, Tensor) -> (Tensor, Tensor) -> IO ((Tensor, Tensor), Tensor)
+linearStep (x, y) slIn = do
+    let y' = linear slIn x
+    displayStep y y'
+    return (slIn, y')
 
 main :: IO ()
 main = do
     let a = asTensor (0.555 :: Float)
     let b = asTensor (94.585026 :: Float)
 
-    let x = asTensor xs
-    let y' = linear (a, b) x
+    let dataSet = zip xs ys
 
-    displayResults (zip ys (asValue y' :: [Float]))
+    (finalParams, ys') <- mapAccumM dataSet (a, b) linearStep
+    print finalParams
+    print ys'
     return ()
